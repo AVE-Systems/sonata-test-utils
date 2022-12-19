ff6a9b67cae0b6ca4ba34d8650337d7d870f0de8
Evgeniya Zemlyanskaya <andomiell@mail.ru>
2022-12-19 16:32:41 +0900

    #8324525 Moved asserts from SonataAdminListBatchActionsTrait to SonataAdminActionsTrait

--- src/SonataAdminActionsTrait.php
+++ src/SonataAdminActionsTrait.php
@@ -56,6 +56,29 @@ trait SonataAdminActionsTrait
     }
 
     /**
+     * Проверяет, что на странице списка присутствует кнопка, соответствующая
+     * переданному групповому действию.
+     *
+     * @param string       $actionTitle
+     * @param Crawler|null $crawler
+     */
+    protected function assertListBatchActionButtonNotExists(
+        string $actionTitle,
+        ?Crawler $crawler
+    ) {
+        $deleteBatchActionXPath = $this->getListBatchActionButtonXPath(
+            $actionTitle
+        );
+
+        $this->assertCount(
+            0,
+            $crawler->filterXPath($deleteBatchActionXPath),
+            sprintf('На странице есть действие "%s"', $actionTitle)
+        );
+    }
+
+
+    /**
      * Возвращает XPath, соответствующий действию на стандартной странице списка
      * Sonata Admin.
      *
@@ -66,5 +89,19 @@ trait SonataAdminActionsTrait
     private function getActionButtonXPath(string $actionTitle): string
     {
         return "//a[contains(@class, 'sonata-action-element') and normalize-space()='$actionTitle']";
+    }
+
+    /**
+     * Возвращает XPath, соответствующий групповому действию на стандартной
+     * странице списка Sonata Admin.
+     *
+     * @param string $actionTitle
+     *
+     * @return string
+     */
+    private function getListBatchActionButtonXPath(
+        string $actionTitle
+    ): string {
+        return "//select[@name='action']/option[.='$actionTitle']";
     }
 }
--- tests/SonataAdminActionsTraitTest.php
+++ tests/SonataAdminActionsTraitTest.php
@@ -124,4 +124,88 @@ HTML;
             $crawler
         );
     }
+
+    public function dataProvider_TestAssertListActions(): array
+    {
+        $html = <<<'HTML'
+<div class="pull-left">
+    <label class="checkbox" for="s1fc55cb054_all_elements">
+        <div class="icheckbox_square-blue">
+            <input type="checkbox" name="all_elements" id="s1fc55cb054_all_elements" style="position: absolute; opacity: 0;">
+            <ins class="iCheck-helper" style="position: absolute; top: 0%; left: 0%; display: block; width: 100%; height: 100%; margin: 0px; padding: 0px; background: rgb(255, 255, 255); border: 0px; opacity: 0;"></ins>
+        </div>
+        Применить для всех (1572)
+    </label>
+    
+    <div class="select2-container" id="s2id_autogen3" style="width: auto; height: auto">
+      <a href="javascript:void(0)" class="select2-choice" tabindex="-1">
+          <span class="select2-chosen" id="select2-chosen-4">
+            Удалить
+          </span>
+          <abbr class="select2-search-choice-close"></abbr>
+          <span class="select2-arrow" role="presentation">
+              <b role="presentation"></b>
+          </span>
+      </a>
+      <a href="javascript:void(0)" class="select2-choice" tabindex="-1">
+          <span class="select2-chosen" id="select2-chosen-4">
+            Показать
+          </span>
+          <abbr class="select2-search-choice-close"></abbr>
+          <span class="select2-arrow" role="presentation">
+              <b role="presentation"></b>
+          </span>
+      </a>
+      <label for="s2id_autogen4" class="select2-offscreen"></label>
+      <input class="select2-focusser select2-offscreen" type="text" aria-haspopup="true" role="button" aria-labelledby="select2-chosen-4" id="s2id_autogen4">
+    </div>
+    
+    <select name="action" style="width: auto; height: auto; display: none;" class="" tabindex="-1" title="">
+      <option value="delete">Удалить</option>
+      <option value="show">Показать</option>
+    </select>
+    
+    <input type="submit" class="btn btn-small btn-primary" value="OK">
+</div>
+HTML;
+
+        return [[$html]];
+    }
+
+    /**
+     * @dataProvider dataProvider_TestAssertListActions
+     *
+     * @param string $html
+     */
+    public function testAssertListBatchActionNotExists_ShouldNotThrowException(string $html)
+    {
+        $crawler = new Crawler();
+        $crawler->addHtmlContent($html);
+
+        $this->assertListBatchActionButtonNotExists(
+            'Редактировать',
+            $crawler
+        );
+    }
+
+    /**
+     * @dataProvider dataProvider_TestAssertListActions
+     *
+     * @param string $html
+     */
+    public function testAssertListBatchActionNotExists_ShouldThrowException(string $html)
+    {
+        $this->expectException(ExpectationFailedException::class);
+        $this->expectExceptionMessageMatches(
+            '.На странице есть действие "Показать".'
+        );
+
+        $crawler = new Crawler();
+        $crawler->addHtmlContent($html);
+
+        $this->assertListBatchActionButtonNotExists(
+            'Показать',
+            $crawler
+        );
+    }
 }
